43463ce7db18ba0a5157c871f3bf34023cf2bb4c
Hrvoje Kuhar <hrkuhar@vsite.hr>
2019-12-02 20:18:35 +0100

    added shopping bag page (in progress)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Items from "../views/Items.vue";
 import ItemDetails from "../views/ItemDetails.vue";
+import ShoppingBag from "../views/ShoppingBag.vue";
 
 Vue.use(VueRouter);
 
@@ -26,6 +27,11 @@ const routes = [
     path: "/item/:id",
     name: "item-details",
     component: ItemDetails
+  },
+  {
+    path: "/shoppingbag",
+    name: "shopping-bag",
+    component: ShoppingBag
   }
 ];
 
---
+++ src/views/ShoppingBag.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    SHOPPING BAG
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingBag",
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
