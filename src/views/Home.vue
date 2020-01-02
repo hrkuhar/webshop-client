d196247a6ed17143ab1dc6115b194ec456fa9733
Hrvoje Kuhar <hrkuhar@vsite.hr>
2020-01-02 13:41:04 +0100

    home page refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <div class="home-background-image" id="background-image-1" />
-    <div class="home-background-image" id="background-image-2" />
-    <div class="home-background-image" id="background-image-3" />
-    <div class="home-background-image" id="background-image-4" />
+    <div id="background-image-1" v-show="showBackground1" />
+    <div id="background-image-2" v-show="showBackground2" />
+    <div id="background-image-3" v-show="showBackground3" />
+    <div id="background-image-4" v-show="showBackground4" />
   </div>
 </template>
 
@@ -12,7 +12,10 @@ export default {
   name: "home",
   data: function() {
     return {
-      shownImage: 1
+      showBackground1: true,
+      showBackground2: false,
+      showBackground3: false,
+      showBackground4: false
     };
   },
   created: function() {
@@ -21,60 +24,40 @@ export default {
     let self = this;
 
     window.setInterval(function() {
-      console.log(self.shownImage);
-
-      if (self.shownImage == 1) {
-        document.getElementById("background-image-2").style.display = "none";
-        document.getElementById("background-image-3").style.display = "none";
-        document.getElementById("background-image-4").style.display = "none";
-        document.getElementById("background-image-1").style.display = "block";
-      } else if (self.shownImage == 2) {
-        document.getElementById("background-image-1").style.display = "none";
-        document.getElementById("background-image-3").style.display = "none";
-        document.getElementById("background-image-4").style.display = "none";
-        document.getElementById("background-image-2").style.display = "block";
-      } else if (self.shownImage == 3) {
-        document.getElementById("background-image-1").style.display = "none";
-        document.getElementById("background-image-2").style.display = "none";
-        document.getElementById("background-image-4").style.display = "none";
-        document.getElementById("background-image-3").style.display = "block";
-      } else if (self.shownImage == 4) {
-        document.getElementById("background-image-1").style.display = "none";
-        document.getElementById("background-image-2").style.display = "none";
-        document.getElementById("background-image-3").style.display = "none";
-        document.getElementById("background-image-4").style.display = "block";
+      if (self.showBackground1) {
+        self.showBackground1 = false;
+        self.showBackground2 = true;
+      } else if (self.showBackground2) {
+        self.showBackground2 = false;
+        self.showBackground3 = true;
+      } else if (self.showBackground3) {
+        self.showBackground3 = false;
+        self.showBackground4 = true;
+      } else if (self.showBackground4) {
+        self.showBackground4 = false;
+        self.showBackground1 = true;
       }
-
-      self.shownImage = self.shownImage == 4 ? 1 : self.shownImage + 1;
     }, 5000);
   },
-  methods: {
-    test: function() {
-      console.log("test");
-    }
-  }
+  methods: {}
 };
 </script>
 
 <style scoped>
 #background-image-1 {
   background: url(../assets/background-home-1.jpg) no-repeat top center;
-  display: block;
 }
 
 #background-image-2 {
   background: url(../assets/background-home-2.jpg) no-repeat bottom center;
-  display: none;
 }
 
 #background-image-3 {
   background: url(../assets/background-home-3.jpg) no-repeat center center;
-  display: none;
 }
 
 #background-image-4 {
   background: url(../assets/background-home-4.jpg) no-repeat center center;
-  display: none;
 }
 
 #background-image-1,
